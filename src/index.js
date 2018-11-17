6f949928cdaed946207ee23505353560485d7bfd
Tikhon Belousko <tikhon.belousko@gmail.com>
2018-11-17 17:43:37 +0300

    Output basic RuleSet.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-/**
- * Export functions you want to work with, see documentation for details:
- * https://github.com/zeplin/zeplin-extension-documentation
- */
+import Layer from 'zeplin-extension-style-kit/elements/layer'
 
 function layer(context, selectedLayer) {
-  return 'Layer Example'
+  const l = new Layer(selectedLayer)
+  const layerStyle = l.style
+  console.log(l.style)
+  return {
+    code: JSON.stringify(layerStyle),
+    language: 'js'
+  }
 }
 
-function styleguideColors(context, colors) {}
-
-function styleguideTextStyles(context, textStyles) {}
-
-function exportStyleguideColors(context, colors) {}
-
-function exportStyleguideTextStyles(context, textStyles) {}
-
-function comment(context, text) {}
+// function styleguideColors(context, colors) {}
+// function styleguideTextStyles(context, textStyles) {}
+// function exportStyleguideColors(context, colors) {}
+// function exportStyleguideTextStyles(context, textStyles) {}
+// function comment(context, text) {}
 
 export default {
-  layer,
-  styleguideColors,
-  styleguideTextStyles,
-  exportStyleguideColors,
-  exportStyleguideTextStyles,
-  comment
+  layer
+  // styleguideColors,
+  // styleguideTextStyles,
+  // exportStyleguideColors,
+  // exportStyleguideTextStyles,
+  // comment
 }
