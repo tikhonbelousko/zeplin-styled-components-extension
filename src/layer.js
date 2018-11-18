5b10fa09769ecaa84ab8ddfa14bb8e2a088ca252
Tikhon Belousko <tikhon.belousko@gmail.com>
2018-11-18 16:27:23 +0300

    Add text style identification.

--- src/layer.js
+++ src/layer.js
@@ -1,25 +1,46 @@
 import Layer from 'zeplin-extension-style-kit/elements/layer'
+import TextStyle from 'zeplin-extension-style-kit/elements/textStyle'
 import Color from 'zeplin-extension-style-kit/values/color'
 import { getUniqueLayerTextStyles } from 'zeplin-extension-style-kit/utils'
+import _ from 'lodash'
 
 function getVariableMap(projectColors) {
-  const variables = {}
-
-  projectColors.forEach(projectColor => {
-    variables[new Color(projectColor).valueOf()] = '${Colors.' + projectColor.name + '}'
-  })
-
-  return variables
+  return projectColors.reduce((variableMap, color) => {
+    variableMap[new Color(color).valueOf()] = '${Colors.' + color.name + '}'
+    return variableMap
+  }, {})
 }
 
-function filterDeclarations(declarations) {
+function filterDeclarations(declarations, textStyleMatch) {
   return declarations.filter(d => {
+    // if (
+    //   ['font-family', 'font-size', 'line-height', 'letter-spacing', 'font-weight'].find(
+    //     v => v === d.name
+    //   )
+    // ) {
+    //   return false
+    // }
+
     return !(d.hasDefaultValue && d.hasDefaultValue())
   })
 }
 
-function declarationsToString(declarations, variableMap) {
-  return `const Element = styled.div\`\n${declarations
+// [ TextStyle {
+//     name: 'Sample text style',
+//     fontFace: 'SFProText-Regular',
+//     fontSize: 20,
+//     fontWeight: 400,
+//     fontStyle: 'normal',
+//     fontFamily: 'SFProText',
+//     fontStretch: 'normal',
+//     textAlign: 'left',
+//     weightText: 'regular',
+//     color: Color { r: 0, g: 0, b: 0, a: 1, name: undefined },
+//     scaledFontSize: 20 },
+function declarationsToString(declarations, variableMap, textStyleMatch) {
+  const filteredDeclarations = filterDeclarations(declarations)
+  const textStyleName = textStyleMatch ? '  ${Typography.' + textStyleMatch.name + '};\n' : ''
+  return `styled.div\`\n${textStyleName}${filteredDeclarations
     .map(d => `  ${d.name}: ${d.getValue({ densityDivisor: 1 }, variableMap)};`)
     .join('\n')}\n\``
 }
@@ -29,16 +50,28 @@ export default function layer(context, selectedLayer) {
   const l = new Layer(selectedLayer)
   const layerRuleSet = l.style
   const { defaultTextStyle } = selectedLayer
+  let textStyleMatch
 
   if (selectedLayer.type === 'text' && defaultTextStyle) {
     const declarations = l.getLayerTextStyleDeclarations(defaultTextStyle)
+    // console.log(new TextStyle(defaultTextStyle).font)
+    const layerTextStyle = new TextStyle(defaultTextStyle).font
+
+    textStyleMatch = projectTextStyles.find(textStyle => {
+      return (
+        textStyle.fontFamily === layerTextStyle.fontFamily &&
+        textStyle.fontSize === layerTextStyle.fontSize &&
+        textStyle.lineHeight === layerTextStyle.lineHeight &&
+        textStyle.letterSpacing === layerTextStyle.letterSpacing
+      )
+    })
+    // console.log(projectTextStyles[0].fontSize, font.fontSize)
+    // console.log(textStyleMatch)
     declarations.forEach(p => layerRuleSet.addDeclaration(p))
-    console.log(declarations)
   }
 
   const variableMap = getVariableMap(projectColors)
-  const filteredDeclarations = filterDeclarations(layerRuleSet.declarations)
-  const code = declarationsToString(filteredDeclarations, variableMap)
+  const code = declarationsToString(layerRuleSet.declarations, variableMap, textStyleMatch)
 
   return {
     code: code,
